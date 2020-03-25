dd625136e8e855242dd34e42dae6556430bec85a
Christopher Grassi <c.grassi@hotmail.fr>
2020-03-25 23:16:46 +0100

    Transparence image et adaptation du canvas à la taille de la photo

--- js/drawImage.js
+++ js/drawImage.js
@@ -1,11 +1,18 @@
 function drawImage(url){
 
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
   var img = new Image();
   img.onload = function(){
 
+    console.log(img.width);
+    console.log(img.height);
+    canvas.height = img.height;
+    canvas.width = img.width;
+
+    ctx.globalAlpha = 0.5;
     ctx.drawImage(img, 0, 0);
+    ctx.globalAlpha = 1;
     faireBlague1partie();
   };
   img.src = url;
